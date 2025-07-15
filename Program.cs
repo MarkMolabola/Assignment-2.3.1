0fe75f66d56d3d180e68eb067c5a0b51871555e1
MarkMolabola <mark.molabola@outlook.com>
2025-07-15 11:44:00 -0700

    update

--- Program.cs
+++ Program.cs
@@ -6,17 +6,30 @@
         {
             //. Write a console application to create a text file and save your basic details like name, age, address ( use dummy data).
             //Read the details from same file and print on console.
-            Console.WriteLine("Hello, World!");
+            
+            string name = "";
+            string age = "";
+            string address = "";
+            string phoneNumber = "";
+
             try
             {
-                string filePath = "TestFile.txt";
+                string filePath = "C:\\Users\\markg\\source\\repos\\Assignment 2.3.1\\TestFile.txt";
                 using (StreamReader sr = new StreamReader(filePath))
                 {
                     string line;
                     // Read and display lines from the file until the end of the file is reached.
                     while ((line = sr.ReadLine()) != null)
                     {
-                        Console.WriteLine(line);
+                        //Console.WriteLine("Name: " + line);
+                        name = line;
+                        line = sr.ReadLine();
+                        age = line;
+                        line = sr.ReadLine();
+                        address = line;
+                        line = sr.ReadLine();
+                        phoneNumber = line;
+
                     }
                 }
             }
@@ -26,6 +39,11 @@
                 Console.WriteLine("Error: " + e.Message);
             }
             
+            Console.WriteLine($"name: {name} ");
+            Console.WriteLine($"age: {age} ");
+            Console.WriteLine($"address: {address} ");
+            Console.WriteLine($"phoneNumber: {phoneNumber} ");
+
         }
     }
 }
